5dcf91870e7e74afeaaf0a34e34e893117b77511
IotaSpencer <me@iotaspencer.me>
2022-01-30 01:21:52 -0500

    "p"

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       user : 'SSH_USERNAME',
       host : 'SSH_HOSTMACHINE',
       ref  : 'origin/master',
-      repo : 'github.com:IotaSpencer/age_bot',
+      repo : 'github.com:IotaSpencer/age_bot_p',
       path : 'DESTINATION_PATH',
       'pre-deploy-local': '',
       'pre-setup': ''
