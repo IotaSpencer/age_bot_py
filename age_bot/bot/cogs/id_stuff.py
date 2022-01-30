c98a56f45f645f1d77ba15a04f262127e1ded3b0
IotaSpencer <me@iotaspencer.me>
2022-01-30 01:56:57 -0500

    "@"

--- age_bot/bot/cogs/id_stuff.py
+++ age_bot/bot/cogs/id_stuff.py
@@ -34,6 +34,7 @@ class IDStuff(commands.Cog, command_attrs=dict(hidden=True)):
 
 
     @slash_command(name="verify")
+    @
     async def slash_verify(self, ctx: ApplicationContext):
 
         ctx.respond('Pong!')
