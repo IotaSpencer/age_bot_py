8f5e7a8462a973362ae827917712a40d6b5e5a1c
IotaSpencer <me@iotaspencer.me>
2022-01-30 01:34:22 -0500

    ""

--- age_bot/bot/cogs/id_stuff.py
+++ age_bot/bot/cogs/id_stuff.py
@@ -34,7 +34,7 @@ class IDStuff(commands.Cog, command_attrs=dict(hidden=True)):
 
 
     @slash_command(name="verify")
-    async def slash_verify(self, parameter_list):
+    async def slash_verify(self, r_list):
         pass
 
 
