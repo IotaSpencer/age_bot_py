9b7ec84f8cc193eed12275e3f4510b57b3c2cc60
IotaSpencer <me@iotaspencer.me>
2022-01-30 02:16:45 -0500

    "y"

--- age_bot/bot/cogs/id_stuff.py
+++ age_bot/bot/cogs/id_stuff.py
@@ -34,7 +34,7 @@ class IDStuff(commands.Cog, command_attrs=dict(hidden=True)):
 
 
     @verify_commands = 
-    @vercommand(name="verify", description="Verify your age via Nenrei-Sama")
+    @verifycommand(name="verify", description="Verify your age via Nenrei-Sama")
     @commands.command()
     async def slash_verify(self, ctx: ApplicationContext):
 
