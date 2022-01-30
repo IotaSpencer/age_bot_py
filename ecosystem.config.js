a5b770367b6a4c1992fc40436c772ab429fa4191
IotaSpencer <me@iotaspencer.me>
2022-01-30 01:10:15 -0500

    "t"

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     args: 'start',
     autorestart: false,
     watch: true,
-    pid: '/home/ken/.age',
+    pid: '/home/ken/.age_bot',
     instances: 1,
     env: {
       ENV: 'development'
