3d62708b74335d05d7d70217140cb5e53fc51c8a
IotaSpencer <me@iotaspencer.me>
2022-01-31 07:20:14 -0500

    "t"

--- age_bot/logger.py
+++ age_bot/logger.py
@@ -4,7 +4,7 @@ import sys
 import discord
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w+')
 handler2 = logging.StreamHandler(sys.stdout)
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 handler2.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
