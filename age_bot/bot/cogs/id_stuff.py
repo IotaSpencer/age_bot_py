ab0281d26fbfe0d0949a6f9cb03412530e8dfee1
IotaSpencer <me@iotaspencer.me>
2022-01-30 23:26:27 -0500

    "h"

--- age_bot/bot/cogs/id_stuff.py
+++ age_bot/bot/cogs/id_stuff.py
@@ -33,7 +33,7 @@ class IDStuff(commands.Cog, command_attrs=dict(hidden=True)):
         self.ext_path = 'age_bot.bot.cogs.id_stuff'
 
 
-    command(name="verify", description="Verify your age via Nenrei-Sama")
+    @slashcommand(name="verify", description="Verify your age via Nenrei-Sama")
     async def slash_verify(self, ctx: ApplicationContext):
 
         ctx.respond('Pong!')
