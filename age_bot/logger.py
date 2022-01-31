61334af98d6d67b89db6505e7b416095e6881b3a
IotaSpencer <me@iotaspencer.me>
2022-01-31 07:18:46 -0500

    "O"

--- age_bot/logger.py
+++ age_bot/logger.py
@@ -3,7 +3,7 @@ from asyncio.subprocess import STDOUT
 import sys
 import discord
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler2 = logging.StreamHandler(sys.stdout)
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
