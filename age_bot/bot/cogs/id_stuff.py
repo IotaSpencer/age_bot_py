c637a9804e5e53567fbf203832013e0ffb6a0155
IotaSpencer <me@iotaspencer.me>
2022-01-31 08:12:51 -0500

    " "

--- age_bot/bot/cogs/id_stuff.py
+++ age_bot/bot/cogs/id_stuff.py
@@ -38,7 +38,7 @@ class IDStuff(commands.Cog, command_attrs=dict(hidden=True)):
 
         if ctx.channel.name == 'hello':
             member = ctx.author
-            verify
+            verify_channel = 
 
 
 
