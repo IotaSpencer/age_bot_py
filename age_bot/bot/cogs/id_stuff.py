2752053e8b3ac47f704c18f922fde3d9489948b0
IotaSpencer <me@iotaspencer.me>
2022-01-31 00:03:09 -0500

    " "

--- age_bot/bot/cogs/id_stuff.py
+++ age_bot/bot/cogs/id_stuff.py
@@ -36,7 +36,7 @@ class IDStuff(commands.Cog, command_attrs=dict(hidden=True)):
     @slash_command(name="verify", description="Verify your age via Nenrei-Sama")
     async def slash_verify(self, ctx: ApplicationContext):
 
-        awactx.respond('Pong!')
+        await ctx.respond('Pong!')
 
 
 
