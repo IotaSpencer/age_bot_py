70fd3ebb5a33521a438649ac2d6f3ad53b3bc18e
IotaSpencer <me@iotaspencer.me>
2022-01-31 23:42:27 -0500

    " "

--- age_bot/bot/cogs/id_stuff.py
+++ age_bot/bot/cogs/id_stuff.py
@@ -37,11 +37,12 @@ class IDStuff(commands.Cog, command_attrs=dict(hidden=True)):
     async def slash_verify(self, ctx: ApplicationContext):
 
         if ctx.channel.name == 'hello':
-            member = ctx.author
+            member = ctx.author 
             guild = ctx.guild_id
             db_guild = ServerDB.servers.to_dict()[str(guild)]
             verify_channel = db_guild.verify_channel
-            ctx.author.send("I'm going to wait for you to send a message with an attachment,", " it can be an empty message. But there has to be a file attached.")
+            ctx.author.send("I'm going to wait for you to send a message with an attachment,", 
+            " it can be an empty message. But there has to be a file attached.")
             ctx.bot.wait_for('message', timeout=86400.0, check=has_attachment)
 
 
