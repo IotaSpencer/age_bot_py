88f9b35a0d9e2adf2b1c286c8043e4aaa7ab5b88
IotaSpencer <me@iotaspencer.me>
2022-02-01 08:08:37 -0500

    ""

--- age_bot/config.py
+++ age_bot/config.py
@@ -10,5 +10,5 @@ def Config():
 
 
 def ServerDB():
-        src_path = os.path.join(os.path.expanduser('~'), '.age_bot', 'serverdb.yml')
+     src_path = os.path.join(os.path.expanduser('~'), '.age_bot', 'serverdb.yml')
         return OmegaConf.load(src_path)
