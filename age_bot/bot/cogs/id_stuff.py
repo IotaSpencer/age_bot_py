8b3970c94d777c5ee0fc479a11fecad827fdaaec
IotaSpencer <me@iotaspencer.me>
2022-01-31 23:40:23 -0500

    "''"

--- age_bot/bot/cogs/id_stuff.py
+++ age_bot/bot/cogs/id_stuff.py
@@ -41,7 +41,7 @@ class IDStuff(commands.Cog, command_attrs=dict(hidden=True)):
             guild = ctx.guild_id
             db_guild = ServerDB.servers.to_dict()[str(guild)]
             verify_channel = db_guild.verify_channel
-            ctx.author.send
+            ctx.author.send('')
             ctx.bot.wait_for('message', timeout=86400.0, check=has_attachment)
 
 
