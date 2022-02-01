18e379acb4e22e8517bf72404cda6e470f7c6914
IotaSpencer <me@iotaspencer.me>
2022-01-31 23:48:05 -0500

    "while waiting for attachment"

--- age_bot/bot/cogs/id_stuff.py
+++ age_bot/bot/cogs/id_stuff.py
@@ -48,7 +48,7 @@ class IDStuff(commands.Cog, command_attrs=dict(hidden=True)):
             try:
                 ctx.bot.wait_for('message', timeout=86400.0, check=has_attachment)
             except asyncio.TimeoutError:
-                ctx.author.send("Timeout")
+                ctx.author.send("Timeout while waiting for attachment")
 
 
     # @commands.command()
