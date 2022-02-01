4b1f04d2d6ce29e510da5587c3f333576a1d8d78
IotaSpencer <me@iotaspencer.me>
2022-02-01 08:08:33 -0500

    ""

--- age_bot/config.py
+++ age_bot/config.py
@@ -5,7 +5,7 @@ from yaml2object import YAMLObject
 from omegaconf import OmegaConf
 
 def Config():
-       src_path = os.path.join(os.path.expanduser('~'), '.age_bot', 'config.yml')
+    src_path = os.path.join(os.path.expanduser('~'), '.age_bot', 'config.yml')
         return OmegaConf.load(src_path)
 
 
