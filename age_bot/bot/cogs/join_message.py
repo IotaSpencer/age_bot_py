b087eee4d499416b025cf17de30b8caba1d1f7d8
IotaSpencer <me@iotaspencer.me>
2022-02-03 09:50:33 -0500

    "n"

--- age_bot/bot/cogs/join_message.py
+++ age_bot/bot/cogs/join_message.py
@@ -23,7 +23,7 @@ class JoinMessage(commands.Cog):
 
 def setup(bot):
     bot.add_cog(JoinMessage(bot))
-    logger.info('Loaded ')
+    logger.info('Loaded Join')
 
 def teardown(bot):
     bot.remove_cog(Fun(bot))
