0edc72cf3a522b7b881ef3c8d38129ae4c64a305
IotaSpencer <me@iotaspencer.me>
2022-02-03 09:45:21 -0500

    "n"

---
+++ age_bot/bot/cogs/join_message.py
@@ -0,0 +1,29 @@
+# built-in
+
+# 3rd party
+import discord
+from discord.ext import commands
+
+# local
+from age_bot.logger import logger
+
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.ext_path = 'age_bot.bot.cogs.jjoin'
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.reply("Pong %sms" % ctx.bot.latency)
+
+
+
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
+    logger.info('Loaded Fun')
+
+def teardown(bot):
+    bot.remove_cog(Fun(bot))
+    logger.info('Unloaded Fun')
