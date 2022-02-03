1c76a44c49d2b06a57c399f745b3520289eb4add
IotaSpencer <me@iotaspencer.me>
2022-02-03 10:08:11 -0500

    add join_message.py

--- age_bot/bot/cogs/join_message.py
+++ age_bot/bot/cogs/join_message.py
@@ -18,6 +18,16 @@ class JoinMessage(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member: Member):
         asyncio.sleep(60)
+        member.send(
+            f"Hello, {message.author}, in order to post or read {message.guild} messages you must be a certain"
+            f" role as well as submitted a form of ID with the server in question. For {message.guild} "
+            f"that role is **{message.guild.get_role(Configs.serverdb.servers[str(message.guild.id)].role).name}**"
+            f"\n\n"
+            f"To do so.. please run the command /verify in #hello and I will message you with further instructions."
+            f"\n\n"
+            f"You may ask questions about the process in #{message.channel} but other than that, "
+            f"non-complying questions or messages will be deleted."
+        )
 
 
 
