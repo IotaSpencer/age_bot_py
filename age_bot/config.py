84334ab09eb838761058297f82f2e240e6299f19
IotaSpencer <me@iotaspencer.me>
2022-06-10 05:35:36 -0400

    add dev config bits

--- age_bot/config.py
+++ age_bot/config.py
@@ -7,10 +7,12 @@ from typing import Union
 from omegaconf import OmegaConf, DictConfig, ListConfig
 
 config_path = os.path.join(os.path.expanduser('~'), '.age_bot', 'config.yml')
+dev_config_path = os.path.join(os.path.expanduser('~'), '.age_bot', 'dev-config.yml')
 sdb_path = os.path.join(os.path.expanduser('~'), '.age_bot', 'serverdb.yml')
+
 
 @dataclass
 class Configs:
     config: Union[DictConfig, ListConfig] = OmegaConf.load(config_path)
+    devconfig: Union[DictConfig, ListConfig] = OmegaConf.load(dev_config_path)
     serverdb: Union[DictConfig, ListConfig] = OmegaConf.load(sdb_path)
-
