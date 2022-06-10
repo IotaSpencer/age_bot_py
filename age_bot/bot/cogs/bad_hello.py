e583e5d8638ec042570b5a58fdd86fd7ed20a136
IotaSpencer <me@iotaspencer.me>
2022-06-10 05:28:59 -0400

    check if tester or main bot

--- age_bot/bot/cogs/bad_hello.py
+++ age_bot/bot/cogs/bad_hello.py
@@ -7,9 +7,10 @@ from discord.ext import commands
 from discord import Message
 
 # local
+from age_bot.bot.helpers.discord import *
 from age_bot.logger import logger
 from age_bot.config import Configs
-
+from age_bot.bot.helpers.decorators import *
 
 class BadHello(commands.Cog):
     def __init__(self, bot):
@@ -18,25 +19,26 @@ class BadHello(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: Message):
-        if message.author.bot is not True:
-            if re.search("^([!@$/]?(verify|hello)\S+)$",
-                         message.content, re.IGNORECASE) and message.channel.name == 'hello':
-                our_message = await message.author.send(
-                    f"Hello, {message.author}, in order to post or read {message.guild} messages you must be a certain"
-                    f" role as well as submitted a form of ID with the server in question. For {message.guild} "
-                    f"that role is **{message.guild.get_role(Configs.serverdb.servers[str(message.guild.id)].role).name}** "
-                    f"\n\n"
-                    f"To do so.. please run the command /verify in #hello and I will message you with further "
-                    f"instructions. "
-                    f"\n\n"
-                    f"You are receiving this message because you messaged #{message.channel} a message that triggered "
-                    f"me.\n "
-                    f"Your message will now be deleted since the message holds no purpose in #{message.channel}."
-                    f"\n\n"
-                    f"You may ask questions about the process in #{message.channel} but other than that, "
-                    f"non-complying questions or messages will be deleted.")
-                await message.delete(delay=0)
-                await our_message.delete(delay=120)
+        if check_if_tester_or_main_bot(message):
+            if message.author.bot is not True:
+                if re.search("^([!@$/]?(verify|hello)\S+)$",
+                             message.content, re.IGNORECASE) and message.channel.name == 'hello':
+                    our_message = await message.author.send(
+                        f"Hello, {message.author}, in order to post or read {message.guild} messages you must be a certain"
+                        f" role as well as submitted a form of ID with the server in question. For {message.guild} "
+                        f"that role is **{message.guild.get_role(Configs.serverdb.servers[str(message.guild.id)].role).name}** "
+                        f"\n\n"
+                        f"To do so.. please run the command /verify in #hello and I will message you with further "
+                        f"instructions. "
+                        f"\n\n"
+                        f"You are receiving this message because you messaged #{message.channel} a message that triggered "
+                        f"me.\n "
+                        f"Your message will now be deleted since the message holds no purpose in #{message.channel}."
+                        f"\n\n"
+                        f"You may ask questions about the process in #{message.channel} but other than that, "
+                        f"non-complying questions or messages will be deleted.")
+                    await message.delete(delay=0)
+                    await our_message.delete(delay=120)
 
 
 def setup(bot: discord.Bot):
--- age_bot/bot/cogs/confirm.py
+++ age_bot/bot/cogs/confirm.py
@@ -3,6 +3,7 @@ import asyncio
 import re
 # 3rd party
 
+import arrow as arw
 from discord.ext import commands
 from discord import Cog
 from discord.ext.commands import Converter, group
@@ -11,16 +12,10 @@ from discord import \
     slash_command
 
 # local
+from age_bot.bot.helpers import AgeConverter
 from age_bot.logger import logger
 from age_bot.bot.helpers.perms_predicate import *
 
-class AgeConverter(Converter):
-    async def convert(self, ctx, argument: str):
-        now = arw.now()
-        d_o_b = arw.get(argument, "DD/MM/YYYY")
-        age = ((now.date().year - d_o_b.date().year) * 372 + (now.date().month - d_o_b.date().month) * 31 + (
-                now.date().day - d_o_b.date().day)) / 372
-        return str(age)
 
 class Confirm(Cog, command_attrs=dict(hidden=True)):
 
@@ -28,84 +23,54 @@ class Confirm(Cog, command_attrs=dict(hidden=True)):
         self.bot = bot
         self.ext_path = 'age_bot.bot.cogs.confirm'
 
-    @group('rej')
-    async def rej(self, ctx):
-        pass
-    @rej.command()
-    async def not_id_or_tag(self, ctx: Context, message: int, user: str, extra: str):
-        member = ctx.guild.get_member_named(user)  # type: Member
-        msg = {}
-        try:
-            msg = await ctx.channel.fetch_message(message)  # type: Message
-        finally:
-            apos = "'" if member.name.endswith('s') else "'s"
-            await ctx.channel.send(f"""{user}{apos} verification rejected, not an ID or discord tag with \"{extra}\" extra context.""")
-            await msg.delete()
-            await member.send(f"Your submission was rejected due to there not being an ID+discordtag in the photo")
-            if extra:
-                await member.send(f"Your rejection was given the following extra context — \"{extra}\"")
-            await member.send(f"If applicable, try again later following the instructions laid out in the rejection "
-                              f"message above.")
-
-    @not_id_or_tag.error
-    async def not_id_or_tag_error(self, ctx, error):
-        if isinstance(error, commands.MissingAnyRole):
-            await ctx.send(
-                f"You are not allowed to use this command, you're missing all of these roles ({error})".format(
-                    error=error.missing_roles))
-        if isinstance(error, commands.MessageNotFound):
-            await ctx.send(
-                f"This message ({error.argument}) does not exist or is too old to delete. If need be, delete the message manually."
-            )
-        if isinstance(error, commands.MemberNotFound):
-            await ctx.send(
-                f"This member ({error.argument}) does not exist, or is not in the cache"
-            )
 
     @slash_command(name="adultify", description="Manually add the 'adult' role to a member",
                    default_permission=False)
     async def slash_adultify(self, ctx: ApplicationContext, user: discord.Member = None, dob: AgeConverter = None):
-        await ctx.defer()
-        age = await AgeConverter.convert(ctx, dob)
+        """[user] [dob]"""
+        if check_if_tester_or_main_bot(ctx):
+            await ctx.defer()
+            author = ctx.user
+            author_roles = author.roles
+            author_named_roles = [role.name for role in author_roles]
+            adult_role = await get_adult_role(ctx)
+            guild = ctx.guild_id
+            db_guild = Configs.serverdb.servers[str(guild)]
+            guild = ctx.guild
+            verify_channel = db_guild.verify_channel
+            channel = await guild.fetch_channel(verify_channel)
+            if has_server_confirm_role(guild, ctx.author):
+                try:
 
-        author = ctx.user
-        author_roles = author.roles
-        author_named_roles = [role.name for role in author_roles]
-        adult_role = await get_adult_role(ctx)
-        guild = ctx.guild_id
-        db_guild = Configs.serverdb.servers[str(guild)]
-        guild = ctx.guild
-        verify_channel = db_guild.verify_channel
-        channel = await guild.fetch_channel(verify_channel)
-        if has_server_confirm_role(guild, ctx.author):
-            try:
+                    await ctx.respond()
 
-                ctx.respond()
-                def check(m: Message):
-                    return re.search(r"""^(Y(es)?|[Nn](o)?)""", m.content) is not None and\
-                           m.author.id == ctx.author.id and m.author.id == ctx.user.id
+                    def check(m: Message) -> bool:
+                        return re.search(r"""^(Y(es)?|[Nn](o)?)""", m.content) is not None and \
+                               m.author.id == ctx.author.id and m.author.id == ctx.user.id
 
-                await ctx.bot.wait_for('message', timeout=60.0, check=check)
-                audit_log_string = f"{ctx.user.mention} manually gave {user.mention} the {adult_role} Role."
-                await user.add_roles(adult_role, reason=audit_log_string)
-                e = discord.Embed(title="Manual Adult")
+                    await ctx.bot.wait_for('message', timeout=60.0, check=check)
+                    audit_log_string = f"{ctx.user.mention} manually gave {user.mention} the {adult_role} Role."
+                    await user.add_roles(adult_role, reason=audit_log_string)
+                    e = discord.Embed(title="Manual Adult")
+                    e.set_author(name=ctx.author)
+                    e.add_field(name="Action", value=audit_log_string)
+                    e.add_field(name="Input", value=f"{dob}")
+                    e.colour = adult_role.color
+                    await ctx.respond(content="Command Triggered.", ephemeral=True)
+                    await channel.send(embed=e)  # sends the embed
+                    await user.send(content=f"You've been confirmed to be a(n) {adult_role.name} on {ctx.guild.name}")
+                except asyncio.TimeoutError:
+                    await ctx.respond(content="Sorry, you have 1 minute to decide. Try again or forget it.")
+
+            else:
+                await ctx.respond(content="You don't have permission to change user roles.")
+                e = discord.Embed(title="Failed Adult")
                 e.set_author(name=ctx.author)
-                e.add_field(name="Action", value=audit_log_string)
-                e.add_field(name="Input", value=f"{dob}")
-                e.colour = adult_role.color
-                await ctx.respond(content="Command Triggered.", ephemeral=True)
-                await channel.send(embed=e)  # sends the embed
-                await user.send(content=f"You've been confirmed to be a(n) {adult_role.name} on {ctx.guild.name}")
-            except asyncio.TimeoutError:
-                await ctx.respond(content="Sorry, you have 1 minute to decide. Try again or forget it.")
-
+                e.add_field(name='No Permission', value="User does not have permission to change user roles")
+                await ctx.respond("You don't have permission to change user roles")
+                await channel.send(embed=e)
         else:
-            await ctx.respond(content="You don't have permission to change user roles.")
-            e = discord.Embed(title="Failed Adult")
-            e.set_author(name=ctx.author)
-            e.add_field(name='No Permission', value="User does not have permission to change user roles")
-            await ctx.respond("You don't have permission to change user roles")
-            await channel.send(embed=e)
+            reply_self_is_dev(ctx)
 
     @commands.command(usage="<message> <user>", description="Confirm an ID as valid")
     @confirmable_check()
@@ -195,3 +160,39 @@ def setup(bot):
 def teardown(bot):
     bot.remove_cog(Confirm(bot))
     logger.info('Unloaded Confirm')
+
+    # @group('rej')
+    # async def rej(self, ctx):
+    #     pass
+    #
+    # @rej.command()
+    # async def not_id_or_tag(self, ctx: Context, message: int, user: str, extra: str):
+    #     member = ctx.guild.get_member_named(user)  # type: Member
+    #     msg = {}
+    #     try:
+    #         msg = await ctx.channel.fetch_message(message)  # type: Message
+    #     finally:
+    #         apos = "'" if member.name.endswith('s') else "'s"
+    #         await ctx.channel.send(
+    #             f"""{user}{apos} verification rejected, not an ID or discord tag with \"{extra}\" extra context.""")
+    #         await msg.delete()
+    #         await member.send(f"Your submission was rejected due to there not being an ID+discordtag in the photo")
+    #         if extra:
+    #             await member.send(f"Your rejection was given the following extra context — \"{extra}\"")
+    #         await member.send(f"If applicable, try again later following the instructions laid out in the rejection "
+    #                           f"message above.")
+    #
+    # @not_id_or_tag.error
+    # async def not_id_or_tag_error(self, ctx, error):
+    #     if isinstance(error, commands.MissingAnyRole):
+    #         await ctx.send(
+    #             f"You are not allowed to use this command, you're missing all of these roles ({error})".format(
+    #                 error=error.missing_roles))
+    #     if isinstance(error, commands.MessageNotFound):
+    #         await ctx.send(
+    #             f"This message ({error.argument}) does not exist or is too old to delete. If need be, delete the message manually."
+    #         )
+    #     if isinstance(error, commands.MemberNotFound):
+    #         await ctx.send(
+    #             f"This member ({error.argument}) does not exist, or is not in the cache"
+    #         )
