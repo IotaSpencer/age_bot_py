ff503e799cc6bd119acc8f130120d5725d81a023
IotaSpencer <me@iotaspencer.me>
2022-06-10 05:34:12 -0400

    add --env / environment flag to cli.py

--- age_bot/cli.py
+++ age_bot/cli.py
@@ -1,24 +1,41 @@
 import os.path
 import asyncclick as click
 import age_bot.bot.client
+from collections.abc import MutableMapping
 
-
-class AgeBotCLI(object):
+class AgeBotCLI(MutableMapping):
     def __init__(self, home=None, debug=False):
+        self.home = home
         self.debug = debug
         home = os.path.expanduser('~') or home
+
+    def __setitem__(self, k, v):
+        setattr(self, k, v)
+    def __getitem__(self, k):
+        getattr(self, k)
+    def __delitem__(self, k):
+        self[k] = None
+    def __iter__(self):
+        raise NotImplementedError
+    def __len__(self):
+        raise NotImplementedError
+
 
 
 @click.group()
 @click.option('--debug/--no-debug', default=False, envvar='AGEBOT_DEBUG')
+@click.option('--env', '-e', default='prod', envvar='AGEBOT_ENV',
+              type=click.Choice(['prod', 'dev'], case_sensitive=False))
 @click.pass_context
-def cli(ctx, debug):
+def cli(ctx, debug, env):
     ctx.obj = AgeBotCLI(debug)
+    ctx.obj.env = env
 
 
 @cli.command()
-async def start():
-    await age_bot.bot.client.start()
+@click.pass_context
+async def start(ctx):
+    await age_bot.bot.client.start(ctx.obj.env)
 
 
 @cli.command()
