69986cc3134b35e12b21034970574912611d4fa6
IotaSpencer <me@iotaspencer.me>
2022-06-10 04:31:19 -0400

    add AgeConverter class into main helpers file

--- age_bot/bot/helpers/__init__.py
+++ age_bot/bot/helpers/__init__.py
@@ -1,8 +1,21 @@
 # built-in
 
 # 3rd party
+from typing import Union
 
+from discord import ApplicationContext
+from discord.ext.commands import Converter, Context
+import arrow as arw
 # local
 
 from . import discord, info_embeds, decorators
 from . import wait_fors
+
+
+class AgeConverter(Converter):
+    async def convert(self, ctx: Union[ApplicationContext, Context], argument: str):
+        now = arw.now()
+        d_o_b = arw.get(argument, "DD/MM/YYYY")
+        age = ((now.date().year - d_o_b.date().year) * 372 + (now.date().month - d_o_b.date().month) * 31 + (
+                now.date().day - d_o_b.date().day)) / 372
+        return str(age)
