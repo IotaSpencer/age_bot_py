e2695c05958cca446e2ad9d6135b53344d8c8e38
IotaSpencer <me@iotaspencer.me>
2022-06-12 23:44:15 -0400

    change the format of the logger.py and add filename, funcname, and lineno

--- age_bot/logger.py
+++ age_bot/logger.py
@@ -6,7 +6,11 @@ logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w+')
 handler2 = logging.StreamHandler(sys.stdout)
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-handler2.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler.setFormatter(logging.Formatter(f"{asctime}:{levelname[0]}:{name}:"
+                                       f"       in {filename}:{funcName}:{lineno!s}"
+                                       f"               {message}"))
+handler2.setFormatter(logging.Formatter(f"{asctime}:{levelname[0]}:{name}:"
+                                        f"       in {filename}:{funcName}:{lineno}"
+                                        f"               {message}"))
 logger.addHandler(handler)
 logger.addHandler(handler2)
