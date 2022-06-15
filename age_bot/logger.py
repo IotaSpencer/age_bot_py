8d37bc11ab15eca8a4cc8ce5537ccf370e8066a9
IotaSpencer <me@iotaspencer.me>
2022-06-14 23:23:31 -0400

    can't use f-string as a format string

--- age_bot/logger.py
+++ age_bot/logger.py
@@ -6,11 +6,11 @@ logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w+')
 handler2 = logging.StreamHandler(sys.stdout)
-handler.setFormatter(logging.Formatter(f"{asctime}:{levelname[0]}:{name}:"
-                                       f"       in {filename}:{funcName}:{lineno!s}"
-                                       f"               {message}"))
-handler2.setFormatter(logging.Formatter(f"{asctime}:{levelname[0]}:{name}:"
-                                        f"       in {filename}:{funcName}:{lineno}"
-                                        f"               {message}"))
+handler.setFormatter(logging.Formatter("{asctime}:{levelname[0]}:{name}:"
+                                       "       in {filename}:{funcName}:{lineno!s}"
+                                       "               {message}"))
+handler2.setFormatter(logging.Formatter("{asctime}:{levelname[0]}:{name}:"
+                                        "       in {filename}:{funcName}:{lineno!s}"
+                                        "               {message}"))
 logger.addHandler(handler)
 logger.addHandler(handler2)
