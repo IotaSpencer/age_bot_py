be37b6332bcfdc829f9afda18293a163295536ec
IotaSpencer <me@iotaspencer.me>
2022-06-15 01:44:17 -0400

    forgot to remove [0] from format string

--- age_bot/logger.py
+++ age_bot/logger.py
@@ -19,10 +19,10 @@ logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w+')
 handler2 = logging.StreamHandler(sys.stdout)
-handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname[0])s:%(name):"
+handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name):"
                                        "       in %(filename)s:%(funcName)s:%(lineno)s"
                                        "               %(message)s"))
-handler2.setFormatter(logging.Formatter("%(asctime)s:%(levelname[0])s:%(name)s:"
+handler2.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:"
                                         "       in %(filename)s:%(funcName)s:%(lineno)s"
                                         "               %(message)s"))
 logger.addHandler(handler)
