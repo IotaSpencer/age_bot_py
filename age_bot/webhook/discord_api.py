9ce27f383c67f27f5650e2296e9f3cbc7fb4a46a
IotaSpencer <me@iotaspencer.me>
2022-07-22 23:27:52 -0400

    remove webhook directory

--- age_bot/webhook/discord_api.py
+++
@@ -1,8 +0,0 @@
-import urllib.parse
-class DAPI:
-    def __init__(self):
-        self.version = 10
-        self.base_url = "https://discord.com/api/"
-
-    def url_for(self, rest):
-        urllib.parse.urljoin(f"{self.base_url}v{self.version}", rest)
