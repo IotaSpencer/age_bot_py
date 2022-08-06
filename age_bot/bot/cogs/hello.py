b25eaea68c43c26c0189b9ee6a1946f73c56b290
IotaSpencer <me@iotaspencer.me>
2022-08-05 23:48:47 -0400

    hello.py: change some things
    * add default permissions to fhello to get it to not show up to other users
    * use get_adult_role to cut down on run-on calling
    * remove references to #upload-example

--- age_bot/bot/cogs/hello.py
+++ age_bot/bot/cogs/hello.py
@@ -33,7 +33,7 @@ class Hello(discord.Cog):
                 f"Please see #id-example for examples on how and format your message."
                 f"\n\n"
                 f"For {ctx.guild.name} that role is "
-                f"{ctx.guild.get_role(Configs.sdb.servers[str(ctx.guild.id)].role)} "
+                f"{get_adult_role(ctx)} "
                 f"\n\n"
                 f"To do so, please run the **command** /verify, and I will message you with further instructions. "
                 f"Also see the #slash-commands channel for info on how to use slash commands."
@@ -41,6 +41,7 @@ class Hello(discord.Cog):
             )
 
     @helper_check()
+    @discord.commands.default_permissions(manage_server=True)
     @bridge.bridge_command(signature='')
     async def fhello(self, ctx: Union[BridgeApplicationContext, BridgeExtContext], user: discord.Member):
         if is_slash_command(ctx):
@@ -48,13 +49,13 @@ class Hello(discord.Cog):
         else:
             pass
         usr = ctx.guild.get_member_named(user)  # type: Member
-        adult_role = ctx.guild.get_role(Configs.sdb.servers[str(ctx.guild.id)].role)
+        adult_role = get_adult_role(ctx)
         await usr.send(
             f"Hello, {user_distinct(usr)}, in order to post or read {ctx.guild.name} messages you must be a "
             f"certain role as well as "
             f"submitted a form of ID with the server in question."
             f"\n\n"
-            f"Please see #id-example and #upload-example for examples on how to upload and format your message.\n"
+            f"Please see #id-example for examples on how to upload and format your message.\n"
 
             f"\n\n"
             f"For {ctx.guild.name} that role is {adult_role} "
@@ -67,16 +68,16 @@ class Hello(discord.Cog):
     async def on_message(self, msg: Message):
         if check_if_tester_or_main_bot(msg, self.bot):
             orig_msg = msg
-            if msg.guild and msg.channel:
-                if msg.channel.name == 'hello':
+            if msg.guild and msg.channel: # check if message is in a guild and has a channel
+                if msg.channel.name == 'hello': # check if message is in the hello channel
                     if message.author.bot is not True and \
-                            re.search("^([!@$/]?(verify|hello))$", message.content, re.IGNORECASE) and \
+                        re.search("^([!@$/]?(verify|hello))$", message.content, re.IGNORECASE) and \
                             message.channel.name == 'hello':
                         try:
                             our_message = await message.author.send(
                                 f"Hello, {message.author}, in order to post or read {message.guild} messages you must be a certain"
                                 f" role as well as submitted a form of ID with the server in question. For {message.guild} "
-                                f"that role is **{message.guild.get_role(Configs.sdb.servers[str(message.guild.id)].role).name}** "
+                                f"that role is **{get_adult_role(ctx)}** "
                                 f"\n\n"
                                 f"To do so.. please run the **command** /verify in #hello and I will message you with further "
                                 f"instructions. Also see the #slash-commands channel for info on how to use slash commands."
@@ -97,7 +98,7 @@ class Hello(discord.Cog):
                                 f"have accept messages from server members ticked.", delete_after=120)
                     elif msg.channel_mentions and msg.author.bot is not True: # Don't react to bots
                         if msg.channel_mentions[0].name == 'hello':
-                            shame_msg = await msg.reply(content="The command is '&hello' nothing else"
+                            shame_msg = await msg.reply(content="The command is '/hello' nothing else"
                                                                 "As per our above rules, as you mentioned the channel, "
                                                                 "you shall be named and shamed.")
                             if find_shamed(msg.guild.text_channels):
